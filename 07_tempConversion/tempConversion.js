2a078b23a74e45d9341cbabdeccecffe4fa4671a
Zuhair Emaz <zuhair.emaz@gmail.com>
2024-12-24 22:34:23 -0700

    Finished exercise 7. Pretty easy! Took only 7 minutes (although I could be faster)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  const newTemp = (temperature - 32) * (5/9);
+  return Math.round(newTemp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  const newTemp = temperature * (9/5) + 32;
+  return Math.round(newTemp * 10) / 10;
 };
 
+console.log(convertToCelsius(32));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
