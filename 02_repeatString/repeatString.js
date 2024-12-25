610b38ce665022fadcef41ad7e19949fec4c43f7
Zuhair Emaz <zuhair.emaz@gmail.com>
2024-12-24 19:37:29 -0700

    Finished exercise 2.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    fullString = "";
 
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+        fullString += string; 
+    }
+    return fullString;
+    } else {
+        return "ERROR";
+    }
+    
 };
 
 // Do not edit below this line
